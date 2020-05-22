8bc099763452127f7f1d997ef15e0b0730790b63
Mathias Bischoff <mathiasbischo@gmail.com>
2020-05-22 10:16:29 +0200

    -

--- my-project/src/Lib.hs
+++ my-project/src/Lib.hs
@@ -7,12 +7,11 @@ module Lib (
 import System.IO
 import Control.Monad
 import System.Random
-import Data.Array.IO
+import Data.List
 
 
-data Player = Blue | Red deriving (Eq, Show)
+data Player = Blue | Red | None deriving (Eq, Show)
 data Status = Running | GameOver deriving (Eq, Show)
-data Winner = Player | None deriving (Eq, Show)
 
 
 type Move = ((Int,Int), (Int, Int), [Char])
@@ -39,6 +38,7 @@ data Game = Game { gameCards :: [Card]
                   , gamePiecesRed :: PieceList
                   , recentMove :: Move
                   , gameState :: Status
+                  , winner :: Player
                   } deriving (Eq, Show)
 
 
@@ -65,15 +65,9 @@ switchPlayer game
   | (gameTurn game) == Red = game {gameTurn = Blue}
 
 
-checkStatus :: Game -> IO (String)
-checkStatus game = do
-  let status = (gameState game)
-  if status == GameOver
-    then return "Game Over"
-    else return "Game Running"
 
 checkTurn :: Game -> Player
-checkTurn (Game _ gt _ _ _ _) = gt
+checkTurn (Game _ gt _ _ _ _ _) = gt
 
 makeMove :: Game -> Int -> Game                                                 -- generate a new random move and update game
 makeMove game seed = do
@@ -87,35 +81,51 @@ makeMove game seed = do
                   else [] ++ [(gameCards game) !! 2] ++ [(gameCards game) !! 3]
 
   let card = chooseRandomCard (mkStdGen seed)
-  let possibleTurns = if ((gameTurn game) == Blue)                              -- set blue or red move possibilities
+  let possibleTurns = if (  (gameTurn game) == Blue)                             -- set blue or red move possibilities
                     then turnsPossible possibleMovesBlue card playerCards
                     else turnsPossible possibleMovesRed card playerCards
 
-  let piece = gamePieces !! (chooseRandomPiece (length gamePieces ) (mkStdGen seed))
-  let move = possibleTurns !! (chooseRandomMove (length possibleTurns) (mkStdGen seed))
+  let piece = gamePieces !! (chooseRandomPiece (length gamePieces) (mkStdGen seed) )
+  let move = possibleTurns !! (chooseRandomMove (length possibleTurns)   (mkStdGen seed) )
   let verifyMove = checkLegalMove gamePieces ((fst piece) + (fst move), (snd piece) + (snd move))
 
-  let theMove = if (verifyMove)                                                 -- if current move is not legal, then we try with another
-                then move
-                else checkLegalGenerate seed possibleTurns gamePieces move
+  let theMove = if (verifyMove == True)                                                 -- if current move is not legal, then we try with another
+                then (move, (playerCards !! card))
+                else do
+                  let mv = checkLegalGenerate seed possibleTurns gamePieces move
+                  if (mv /= move)                                               -- if mv is not initial move, it means we have found another legal move alternative with this card.
+                    then (mv, (playerCards !! card))
+                    else do                                                     -- this means that the initial move was not legal, and no legal move was left for this card. We try the other.
+                      let newCard = removeItem (playerCards !! card) playerCards -- size of this can only be one  (index 0) as we "remove" other card
+                      let possibleTurns2 = if ( ( gameTurn game) == Blue )
+                                          then turnsPossible possibleMovesBlue 0 newCard
+                                          else turnsPossible possibleMovesRed 0 newCard
+                      let move2 = possibleTurns2 !! (chooseRandomMove (length possibleTurns2)  (mkStdGen seed) )
+                      let verifyMove2 = checkLegalMove gamePieces ((fst piece) + (fst move2), (snd piece) + (snd move2))
+                      if (verifyMove2)
+                        then (move2, (newCard !! 0))
+                        else (checkLegalGenerate seed possibleTurns2 gamePieces move2, (newCard !! 0))
 
-  let newPiece = if (checkLegalMove gamePieces ((fst piece)+(fst theMove), (snd piece)+(snd theMove)))
-                then ((fst piece)+(fst theMove), (snd piece)+(snd theMove))
+  -- MAKE SOME FUNCTION FOR OTHER CARD TMRW
+  let tupMove = (fst theMove)
+  let actualCard = (snd theMove)
+
+  let newPiece = if (checkLegalMove gamePieces ((fst piece)+(fst tupMove), (snd piece)+(snd tupMove)))
+                then ((fst piece)+(fst tupMove), (snd piece)+(snd tupMove))
                 else piece
+
 
   let newGamePieces = removeItem piece gamePieces
 
-  let fullMove = (piece, newPiece, (playerCards !! card))
+  let fullMove = (piece, newPiece, actualCard)
                                                                                 -- update game variables
-  let initialWPieces = updatePieces game (chooseRandomPiece (length gamePieces ) (mkStdGen seed)) newGamePieces newPiece
+  let initialWPieces = updatePieces game (chooseRandomPiece (length gamePieces) (mkStdGen seed) ) newGamePieces newPiece
   let initialOpPiece = takePiece initialWPieces newPiece
-  let initialWCards = updateCards initialOpPiece card playerCards
+  let initialWCards = updateCards initialOpPiece actualCard playerCards
   let initialWTurn = switchPlayer initialWCards
   let newGame = updateMove initialWTurn fullMove
   newGame
 
--- TODO: hvis vi fjerner sensei fra takePieces, sæt current spiller som vinder.
--- TODO: hvis vi ikke har flere muligheder mht. legal moves for det aktuelle kort, så prøv for det andet.
 
 checkLegalGenerate :: Int -> PieceList -> PieceList -> Coordinate -> Coordinate -- Finds a legal move if current move is not legal.
 checkLegalGenerate seed xyz xy mv = do
@@ -128,30 +138,28 @@ checkLegalGenerate seed xyz xy mv = do
                 then (head xyz)
                 else do
                   let newArr = removeItem mv xyz
-                  let newMv = newArr !! (chooseRandomMove (length newArr) (mkStdGen seed))
+                  let newMv = newArr !! (chooseRandomMove (length newArr) (mkStdGen seed) )
                   checkLegalGenerate seed newArr xy newMv
   move
 
 
-
 checkLegalMove :: PieceList -> Coordinate -> Bool
-checkLegalMove xy mv
-  | (mv `elem` xy) = False                                                      -- If the move is already in the pieceList, then we cannot make this move, because else we will have 2 pieces on same spot.
+checkLegalMove xy mv                                                            -- If the move is already in the pieceList, then we cannot make this move, because else we will have 2 pieces on same spot.
   | (fst mv) > 4 = False                                                        -- check if xy out of bounds
   | (fst mv) < 0 = False                                                        --------
   | (snd mv) > 4 = False                                                        --------
-  | (snd mv) < 0 = False                                                        --------
+  | (snd mv) < 0 = False
+  | (mv `elem` xy) = False                                                        --------
   | otherwise = True
 
 
-updateCards :: Game -> Int -> [Card] -> Game
-updateCards gm cd cards = do
-  let cardName = (cards !! cd)
+updateCards :: Game -> String -> [Card] -> Game
+updateCards gm cardName cards = do
   let arr = removeItem cardName cards
-  let newPlayerCards = arr ++ [(last(gameCards gm))]
+  let newPlayerCards = sort (arr ++ [(last(gameCards gm))])
   let opponentCards = if ((gameTurn gm) == Blue)
-                   then [] ++ [(gameCards gm) !! 2] ++ [(gameCards gm) !! 3]
-                   else [] ++ [(gameCards gm) !! 0] ++ [(gameCards gm) !! 1]
+                   then sort ([] ++ [(gameCards gm) !! 2] ++ [(gameCards gm) !! 3])
+                   else sort ([] ++ [(gameCards gm) !! 0] ++ [(gameCards gm) !! 1])
   let newArray = if ((gameTurn gm) == Blue)
                 then newPlayerCards ++ opponentCards ++ [cardName]
                 else opponentCards ++ newPlayerCards ++ [cardName]
@@ -169,27 +177,36 @@ turnsPossible possibleMoves n arr
 
 takePiece :: Game -> Coordinate -> Game
 takePiece gm xy = do
-  let opponentXY = if ((gameTurn gm) == Blue)
+  let opponentXY = if ( (gameTurn gm) == Blue)
                   then (gamePiecesRed gm)
                   else (gamePiecesBlue gm)
-  let removePiece = if ((xy `elem` opponentXY) == True)                         -- if the piece is in opponent array
+  let removePiece = if ( (xy `elem` opponentXY) == True)
                     then xy
-                    else (99,99)                                                -- set to something invalid out of bounds, which will not remove anything from array ..
+                    else (99,99)
+  let senseiRemove = if ((opponentXY !! 0) == removePiece)
+                      then True
+                      else False
   let newOpponentXY = removeItem removePiece opponentXY
-  if ((gameTurn gm) == Blue)
-    then gm {gamePiecesRed = newOpponentXY}
-    else gm {gamePiecesBlue = newOpponentXY}
+  setOpponentXY gm senseiRemove newOpponentXY
+
+setOpponentXY :: Game -> Bool -> [Coordinate] -> Game
+setOpponentXY gm sensei arr
+  | ((gameTurn gm) == Blue && sensei == True) = gm {gamePiecesRed = arr, gameState = GameOver, winner = Blue} -- sensei was killed, set winner to whoevers turn it is
+  | ((gameTurn gm) == Red && sensei == True) = gm {gamePiecesBlue = arr, gameState = GameOver, winner = Red}
+  | ((gameTurn gm) == Blue) = gm {gamePiecesRed = arr}
+  | ((gameTurn gm) == Red) = gm {gamePiecesBlue = arr}
+
 
 updatePieces :: Game -> Int -> [(Int,Int)] -> (Int, Int) -> Game
-updatePieces gm ind arr newPiece                                                -- ind = 0 means sensei piece, 1 means pawn
+updatePieces gm ind arr newPiece
   | ind == 0 && (gameTurn gm == Blue) =
-    gm { gamePiecesBlue = newPiece:arr }
+    gm { gamePiecesBlue = newPiece:(sort(arr)) }
   | ind == 0 && (gameTurn gm == Red) =
-    gm { gamePiecesRed = newPiece:arr }
+    gm { gamePiecesRed = newPiece:(sort(arr)) }
   | ind /= 0 && (gameTurn gm == Blue) =
-    gm { gamePiecesBlue = arr ++ [newPiece] }
+    gm { gamePiecesBlue = ([head arr] ++ sort(tail(arr) ++ [newPiece])) }
   | ind /= 0 && (gameTurn gm == Red) =
-    gm { gamePiecesRed = arr ++ [newPiece] }
+    gm { gamePiecesRed = ([head arr] ++ sort(tail(arr) ++ [newPiece])) }
 
 updateMove :: Game -> Move -> Game
 updateMove gm mv = gm { recentMove = mv }
@@ -228,6 +245,9 @@ randomInitial gen =
       string5 = (newList4 !! 0)                                                 -- size will be 1 and we will get last element available
   in  [string1, string2, string3, string4, string5]
 
+finalizeArr :: [String] -> [String]
+finalizeArr x = ( (sort([x !! 0] ++ ([x !! 1]))) ++ (sort([x !! 2] ++ ([x !! 3]))) ++ [x !! 4])
+
 ---------- Remove item in array -------------
 removeItem _ []                 = []
 removeItem x (y:ys) | x == y    = removeItem x ys
@@ -253,23 +273,38 @@ initt = Game { gameCards = randomInitial (mkStdGen 100)
                     , gamePiecesBlue = [(0,2),(0,0),(0,1),(0,3),(0,4)]
                     , gamePiecesRed = [(4,2),(4,0),(4,1),(4,3),(4,4)]
                     , recentMove = ((0,0),(0,0),"null")
-                    , gameState = Running }
+                    , gameState = Running
+                    , winner = None}
+
+lol1 = makeMove initt 100
+lol2 = makeMove lol1 100
+lol3 = makeMove lol2 100
+lol4 = makeMove lol3 100
+lol5 = makeMove lol4 100
+lol6 = makeMove lol5 100
+lol7 = makeMove lol6 100
+lol8 = makeMove lol7 100
+lol9 = makeMove lol8 100
+lol10 = makeMove lol9 100
+lol11 = makeMove lol10 100
+lol12 = makeMove lol11 100
+
 
 -------------------------------------------------------
 --------------- FUNCTIONALITY -------------------------
 generateRandom :: Int -> Int -> IO (String)
 generateRandom x y = do
-  let game = Game { gameCards = randomInitial (mkStdGen x)
+  let game = Game { gameCards = finalizeArr (randomInitial (mkStdGen x))
                       , gameTurn = Blue
                       , gamePiecesBlue = [(0,2),(0,0),(0,1),(0,3),(0,4)]
                       , gamePiecesRed = [(4,2),(4,0),(4,1),(4,3),(4,4)]
                       , recentMove = ((0,0),(0,0),"null")
-                      , gameState = Running }
+                      , gameState = Running
+                      , winner = None}
 
   let initialTable = printTable game
   let game1 = makeMove game x
-  let p1 = printMove game1
-  let total = gameLoop x y game1 "" -- append to empty string
+  let total = gameLoop x y game1 ""                                             -- append to empty string
   return (initialTable ++ total)
 
 -------------------------------------------------------
@@ -277,12 +312,14 @@ generateRandom x y = do
 gameLoop :: Int -> Int -> Game -> String -> String
 gameLoop _ 0 _ s = s                                                            -- return final string when n==0
 gameLoop seed n gm string  | (gameState gm) == GameOver = "Game Over."
+                           | (winner gm) == Blue = "Blue Wins."
+                           | (winner gm) == Red = "Red Wins."
                            | otherwise = do
 
     let move = printMove gm
     let newString = string ++ move
     let newGame = makeMove gm seed
-    gameLoop (seed+1) (n-1) newGame newString
+    gameLoop (seed) (n-1) newGame newString
 ----------------------------------------------------------
 
 isValid :: FilePath -> IO (String)
